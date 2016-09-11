98666f1949276171d2d52bf9ff570c1555bdcc07
ben yang <benyang00@gmail.com>
2016-09-11 13:47:07 +0800

    created relationship model

--- app/models/rs.server.model.js
+++ app/models/rs.server.model.js
@@ -1,17 +1,21 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-var InvSchema = new Schema({
-  sender_id: [
+var RsSchema = new Schema({
+  user1_id: [
     {
       type: Schema.Type.ObjectId, ref: 'User'
     }
   ],
-  receiver_id: [
+  user2_id: [
     {
       type: Schema.Type.ObjectId, ref: 'User'
     }
-  ]
+  ],
+  rs_type: {
+    type: Number,
+    required: true
+  }
 });
 
-mongoose.model('Inv', UserSchema);
+mongoose.model('Rs', RsSchema);
